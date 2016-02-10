be868d8ad5f01527aebe00f8513b6ec0e224d2a2
J. Brant Carson <bcarson408@gmail.com>
2016-02-10 12:07:16 -0800

    Initial Commit

---
+++ UsingCout/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  UsingCout
+//
+//  Created by J. Brant Carson on 2/10/16.
+//  Copyright © 2016 J. Brant Carson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
